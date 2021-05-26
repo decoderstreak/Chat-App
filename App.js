55c022443d4f58f1dc626bb1b184804054ae4fe6
Decoder streak <uman.tab12@gmail.com>
2021-05-26 16:41:39 +0530

    commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+class App extends React.Component{
+  render()
+  {
+    return(
+      <View>
+        <Text>Let's create a chat App</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
